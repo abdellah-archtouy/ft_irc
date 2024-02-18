d115200554eb895ee4d8e1c80331b54590a5b4d0
TalalMiftah <talalmiftahvrry@gmail.com>
2024-02-18 16:25:25 +0100

    done in the god willing

--- commands/invite.cpp
+++ commands/invite.cpp
@@ -7,6 +7,7 @@ void invite(std::vector<std::string> command, Server &s, int socket) {
     std::vector<Channels>::iterator itrChaine = findChaine(command[2], s.Channel);
     if (itrChaine == s.Channel.end())
         return sendError(ERR_NOSUCHCHANNEL(s.get_host(), s.get_clients()[socket]->get_nickname(), command[2]), socket);
+
     if (itrChaine->get_i() && std::find(itrChaine->getOperators().begin(), itrChaine->getOperators().end(), socket) == itrChaine->getOperators().end())
         return sendError(ERR_CHANOPRIVSNEEDED(s.get_host(), command[2], s.get_clients()[socket]->get_nickname()), socket);
         
@@ -17,11 +18,11 @@ void invite(std::vector<std::string> command, Server &s, int socket) {
     std::map<int, User *>::iterator user = findFromAllChannels(s.get_clients(), command[1]);
     if (user == s.get_clients().end())
         return sendError(ERR_NOSUCHNICK(s.get_host(), s.get_clients()[socket]->get_nickname(), command[1]), socket);
+
     itrUser = itrChaine->getUsers().find(user->first);
     if (itrUser != itrChaine->getUsers().end())
         return sendError(ERR_USERONCHANNEL(s.get_host(), s.get_clients()[socket]->get_nickname(), command[1], command[2]), socket);
 
-    itrChaine->setUsers(user->first, user->second->get_nickname());
     itrChaine->set_inviteList(user->first); // here we add socket to invite list
     std::string str = ":" + FORMA(s.get_clients()[socket]->get_username(), s.get_clients()[socket]->get_nickname(), s.get_clients()[socket]->get_ip())
         + " 341 " + user->second->get_nickname() + " " + s.get_clients()[socket]->get_nickname() + " " + itrChaine->getName() + "\r\n";
